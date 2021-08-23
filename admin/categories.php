611a25e6a9d23cc1d3db9c8913981b24d471f5f5
Jonathan <jonathanrautenbach@gmail.com>
2021-08-23 15:43:17 +0200

    Refactoring and Posts page Admin

--- admin/categories.php
+++ admin/categories.php
@@ -17,19 +17,7 @@
                             Admin Page
                         </h1>
                         <div class="col-xs-6">
-                            <?php if (isset($_POST['submit'])) {
-                                $cat_title = $_POST['cat_title'];
-                                if ($cat_title == "" || empty($cat_title)) {
-                                    echo "<div class='row'><div class='alert alert-danger col-xs-6' role='alert'> This field should not be empty!</div></div>";
-                                } else {
-                                    $query = "INSERT INTO categories(cat_title) ";
-                                    $query .= "VALUE('{$cat_title}') ";
-                                    $create_category_query = mysqli_query($connection, $query);
-                                    if (!$create_category_query) {
-                                        die('QUERY FAILED' . mysqli_error($connection));
-                                    }
-                                }
-                            }  ?>
+                            <?php insert_categories(); ?>
                                 
                             
                             <form action="" method="post">
@@ -42,13 +30,13 @@
                                 </div>
                             </form><!-- Add category form -->
 
-                            <?php 
+                            <!-- Update and include query --><?php 
                             if (isset($_GET['edit'])) {
                                 $cat_id = $_GET['edit'];
                             
                             include "includes/update_categories.php"; 
                             }
-                        ?><!-- Edit category form -->
+                        ?><!-- EUpdate and include query -->
 
                         </div>
                         <div class="col-xs-6">
@@ -60,31 +48,8 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php  
-                                    //Find All Categories Query
-                                    $query = "SELECT * FROM categories";
-                                            $select_categories = mysqli_query($connection, $query);
-
-                                            while ($row = mysqli_fetch_assoc($select_categories)) {
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-                                            echo "<tr>";
-                                            echo "<td>{$cat_id}</td>";
-                                            echo "<td>{$cat_title}</td>";
-                                            echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                            echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                            echo "</tr>";
-                                    } ?>
-                                    
-                                    <?php 
-                                    //DELETE query
-                                    if (isset($_GET['delete'])) {
-                                        $the_cat_id = $_GET['delete'];
-                                        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                                        $delete_query = mysqli_query($connection,$query);
-                                        header("Location: categories.php");
-                                    }
-                                     ?>    
+                                    <?php findAllCategories(); ?>
+                                    <?php deleteCategories(); ?>    
                                         
                                 </tbody>
                             </table>
