52288333ca4c313ff2c74ab5cc62c113af47ae31
Jonathan <jonathanrautenbach@gmail.com>
2021-08-26 13:38:55 +0200

    Reading posts from DB

--- admin/posts.php
+++ admin/posts.php
@@ -19,34 +19,7 @@
                         <h1 class="page-header">
                             Posts
                         </h1>
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Post ID</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>Image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>10</td>
-                                <td>Jonathan Rautenbach</td>
-                                <td>Title</td>
-                                <td>Category</td>
-                                <td>Status</td>
-                                <td>Image</td>
-                                <td>Tags</td>
-                                <td>Comment</td>
-                                <td>Date</td>
-                            </tr>
-                        </tbody>
-                        </table>
+                        
                         
                     </div>
                 </div>
