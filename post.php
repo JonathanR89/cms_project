f3f8bda3780fdc443f4f26fc3442b85fecdac8d0
Jonathan <jonathanrautenbach@gmail.com>
2024-02-29 11:23:22 +0200

    Adjusted image Place holder URL

--- post.php
+++ post.php
@@ -114,7 +114,7 @@
                      <!-- PHP Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
-                        <img class="media-object" src="https://via.placeholder.com/64" alt="">
+                        <img class="media-object" src="https://placehold.co/64" alt="">
                     </a>
                     <div class="media-body">
                         <h4 class="media-heading"><?php echo $comment_author ?>
